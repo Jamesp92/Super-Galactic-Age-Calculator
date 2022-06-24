0e0315a3cd279fbbecfe3d6e0014b71daf48023a
pj perry <pjperry28@gmail.com>
2022-06-24 13:43:03 -0700

    write and fail test for mars

--- __tests__/biz-func.test.js
+++ __tests__/biz-func.test.js
@@ -9,4 +9,8 @@ describe('PlanetCalculator', () => {
     const years = new PlanetCalculator(10)
   expect(years.venus).toEqual(6.2);
  });
+ test('should take user age and calculate it to match age on another planet', () => {
+  const years = new PlanetCalculator(10)
+  expect(years.mars).toEqual(18.8);
+  });
 });
