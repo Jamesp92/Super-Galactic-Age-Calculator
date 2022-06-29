4c5d396efa56cc227857201c9073a333a62845d9
pj perry <pjperry28@gmail.com>
2022-06-28 22:40:30 -0700

    wip working on life expectancy

--- src/js/biz-func.js
+++ src/js/biz-func.js
@@ -1,13 +1,19 @@
 import $ from 'jquery';
 export default class PlanetCalculator{
-  constructor() {
-    this.personAge = 10;
+  constructor(personAge) {
+    this.maxLife = 70;
+    this.personAge = personAge;
+    this.currentAge = this.mercury || this.venus || this.mars || this.jupiter ;
+    this.lifeExpect = this.currentAge - this.maxLife;
     this.mercury = this.personAge *.24 ;
     this.venus = this.personAge * .62;
     this.mars =  this.personAge * 1.88;
     this.jupiter = this.personAge *  11.86;
   }
+   lifeExpectCalc(){
+    this.jupiter + this.lifeExpect;
+   }
 
-}
+  }
 
 
